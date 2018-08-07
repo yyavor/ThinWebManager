df6a1e9e8c685cbd3c7bb5ff22ebee4774913190
yyavor <yyavorovskyi@gmail.com>
2018-08-07 16:20:41 +0300

    Product creation forms widgets usage

--- ThinWebManagerEnv/src/thinwebmanager/products/forms.py
+++ ThinWebManagerEnv/src/thinwebmanager/products/forms.py
@@ -3,6 +3,17 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder": "Product Title"}))
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={
+        "placeholder": "Product Description",
+        "class": "new-class-name",
+        "id": "my-text-area-id",
+        "rows": 20,
+        "cols": 120
+    }
+    ))
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = [
@@ -10,6 +21,12 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "TEST" in title:
+            return title
+        raise forms.ValidationError("This is not a valid title")
 
 
 class RawProductForm(forms.Form):
