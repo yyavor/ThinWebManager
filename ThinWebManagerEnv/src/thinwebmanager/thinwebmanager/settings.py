a3e606b1fdb97fc1f33f3c600e1793861d8b90d7
yyavor <yyavorovskyi@gmail.com>
2018-08-13 15:02:57 +0300

    Update chat functionality

--- ThinWebManagerEnv/src/thinwebmanager/thinwebmanager/settings.py
+++ ThinWebManagerEnv/src/thinwebmanager/thinwebmanager/settings.py
@@ -57,6 +57,15 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'thinwebmanager.urls'
 ASGI_APPLICATION = "thinwebmanager.routing.application"
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- ThinWebManagerEnv/src/thinwebmanager/thinwebmanager/urls.py
+++ ThinWebManagerEnv/src/thinwebmanager/thinwebmanager/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
     path('products/', include('products.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^chat/', include('chat.urls')),
     path('', home_view, name='home'),
     path('about/', about_view),
     path('contact/', contact_view),
